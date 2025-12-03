c4d16aeb25fa571a3459a4a4cd30515d0e33df82
sadan <117494111+sadan4@users.noreply.github.com>
2025-12-03 12:50:56 -0500

    d3p{1..2}

---
+++ src/bin/03.rs
@@ -0,0 +1,85 @@
+advent_of_code::solution!(3);
+
+pub fn parse_input(s: &str) -> impl Iterator<Item = &[u8]> {
+    s.trim().split('\n').map(|line| line.as_bytes())
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    return Some(parse_input(input).fold(0u64, |acc, cur| acc + highest_combo(cur) as u64));
+    fn highest_combo(ints: &[u8]) -> u8 {
+        let mut first = ints[0] - '0' as u8;
+        let mut idx = 0;
+        let mut second = 0;
+        let len = ints.len();
+        for i in 1..len - 1 {
+            let cur = ints[i] - ('0' as u8);
+            if cur == 9 {
+                first = 9;
+                idx = i;
+                break;
+            }
+            if cur > first {
+                first = cur;
+                idx = i;
+            }
+        }
+        for i in idx + 1..len {
+            let cur = ints[i] - ('0' as u8);
+            if cur == 9 {
+                return first * 10 + cur;
+            }
+            if cur > second {
+                second = cur;
+            }
+        }
+        return first * 10 + second;
+    }
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    return Some(parse_input(input).fold(0u64, |acc, cur| acc + highest_combo(cur) as u64));
+    fn highest_combo(ints: &[u8]) -> u64 {
+        let mut result = 0;
+        let mut last_idx = 0;
+        for step in (0..=11).rev() {
+            let possible = &ints[last_idx..ints.len() - step];
+            let cur_idx = last_idx + max_idx(possible);
+            result += (ints[cur_idx] - '0' as u8) as u64 * 10u64.pow(step as u32);
+            last_idx = cur_idx + 1;
+        }
+        return result;
+    }
+    fn max_idx(arr: &[u8]) -> usize {
+        let mut max = 0;
+        let mut max_idx = 0;
+        for i in 0..arr.len() {
+            let cur = arr[i];
+            if cur == '9' as u8 {
+                return i;
+            }
+            if cur > max {
+                max = cur;
+                max_idx = i;
+            }
+        }
+        return max_idx;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(17766));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        // sample result
+        assert_eq!(result, Some(176582889354075));
+    }
+}
